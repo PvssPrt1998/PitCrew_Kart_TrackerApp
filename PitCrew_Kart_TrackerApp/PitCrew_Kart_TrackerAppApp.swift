23b3d2019680fd63c7accf7d2b2f7620187b5382
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-07-19 16:23:12 +0500

    Initial Commit

---
+++ PitCrew_Kart_TrackerApp/PitCrew_Kart_TrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitCrew_Kart_TrackerAppApp.swift
+//  PitCrew_Kart_TrackerApp
+//
+//  Created by Николай Щербаков on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PitCrew_Kart_TrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
